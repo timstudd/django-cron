33e2d4f6769b427c81b37c352d908320b7a65321
Tim Studd <tim@timstudd.com>
2013-11-11 15:42:39 -0600

    Removed signal termination of scheduler

--- django_cron/__init__.py
+++ django_cron/__init__.py
@@ -20,12 +20,7 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 """
-import signal
-
-from base import Job, cronScheduler, abortExecution
-
-signal.signal(signal.SIGINT, abortExecution)
-signal.signal(signal.SIGTERM, abortExecution)
+from base import Job, cronScheduler
 
 def autodiscover():
 	"""
